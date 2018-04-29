bd5592578b96aa62f59b0d7f302ed644a06a1d4c
Christian Altamirano Ayala <christian.altamirano.ayala@gmail.com>
2018-04-29 15:41:00 -0500

    correcion

--- src/main/java/com/celmam/eureka/RegistryServiceApplication.java
+++ src/main/java/com/celmam/eureka/RegistryServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class RegistryServiceApplication {
 
-	public static void main(String[] strings) {
+	public static void main(String[] args) {
 		SpringApplication.run(RegistryServiceApplication.class, args);
 	}
 }
